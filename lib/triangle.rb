fc863a05b43d543a2507134c7a6dccabba2dcb2e
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-04 20:39:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,11 +15,11 @@ class Triangle
 
   def kind
     if @a == @b && @b == @c
-@kind = "equilateral"
+@kind = :equilateral
     elsif @a < @b && @b == @c
-@kind = "isosceles"
+@kind = :isosceles
     elsif @a < @b && @b < @C
-@kind = "scalene"
+@kind = :scalene
     elsif (@a + @b) > @c != true
       raise TriangleError
     end
