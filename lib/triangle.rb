856bb14a983434311c036c4c5504f655f4b5d7d8
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-04 20:32:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
 @kind = "isosceles"
     elsif @a < @b && @b < @C
 @kind = "scalene"
-    elsif (@a + @b) !> @c
+    elsif (@a + @b) > @c != true
       raise TriangleError
     end
   end
